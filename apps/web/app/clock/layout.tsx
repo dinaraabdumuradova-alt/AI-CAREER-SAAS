60f8dcc2cbfbab45df7fd515bf2178a01af2715c
dinaraabdumuradova-alt <dinaraabdumuradova@gmail.com>
2026-05-15 13:14:48 +0500

    Create clock page wrapper

---
+++ apps/web/app/clock/layout.tsx
@@ -0,0 +1,10 @@
+import DigitalClock from './DigitalClock';
+
+export const metadata = {
+  title: 'World Clock | AI Career SAAS',
+  description: 'Digital clock displaying current time in different time zones',
+};
+
+export default function ClockPage() {
+  return <DigitalClock />;
+}
